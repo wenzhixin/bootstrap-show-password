f3e65b3bd4ca1d1521fa95aacbfa392676ee0d31
Travis CI <wenzhixin2010@gmail.com>
2019-05-11 09:24:18 +0000

    Deploy to GitHub Pages: 75a3a09fdcde10480b415a50ca420800c07385fa

--- assets/js/examples.js
+++ assets/js/examples.js
@@ -9,7 +9,7 @@ function loadUrl(url) {
   } else if (location.search.slice(1) === 'is-debug') {
     hash = '#is-debug'
   }
-  $('iframe').attr('src', template + '?v=3&' + url + hash)
+  $('iframe').attr('src', template + '?v=4&' + url + hash)
 }
 
 function initNavigation(href) {
